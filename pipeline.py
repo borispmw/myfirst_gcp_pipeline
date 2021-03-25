f0d2bf549031a4b2e9921a5d9841fa89e3bc291e
borispmw <borispmw@gmail.com>
2021-03-25 19:44:22 +0200

    upd 1

---
+++ pipeline.py
@@ -0,0 +1,15 @@
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+#from apache_beam.io import ReadFromAvro
+#from apache_beam.io import WriteToText
+
+
+class MyOption(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument('--input')
+        parser.add_argument('--output')
+
+with beam.Pipeline(options=PipelineOptions()) as p:
+    lines = p | 'Read' >> beam.io.ReadFromAvro(input)
+    lines | 'WriteToText' >> beam.io.WriteToText(output, file_name_suffix='.csv')
