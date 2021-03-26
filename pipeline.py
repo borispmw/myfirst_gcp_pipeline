db0cfe48fda3c69f39c5593292aa9c8286f9564f
borispmw <borispmw@gmail.com>
2021-03-26 19:39:55 +0200

    upd? added input output cl parameters

--- pipeline.py
+++ pipeline.py
@@ -1,17 +1,34 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-#from apache_beam.io import ReadFromAvro
-#from apache_beam.io import WriteToText
+import argparse
+import logging
+
+def run(argv=None, save_main_session=True):  
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input',
+        dest='input',
+        default='gs://dataflow-samples/shakespeare/kinglear.txt',
+        help='Input file to process.')
+    parser.add_argument(
+        '--output',
+        dest='output',
+        required=True,
+        help='Output file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    def formatout(x):
+        return '{},{},{}'.format(x[0],x[1],x[2])
 
 
-def formatout(x):
-    return '{},{},{}'.format(x[0],x[1],x[2])
+    with beam.Pipeline(options=PipelineOptions(pipeline_args)) as p:
+        lines = (p | 'ReadData' >> beam.io.ReadFromAvro(known_args.input)#'gs://temp_data_exam/usa.avro')
+        | 'MapData' >> beam.Map(lambda record: (record['year'],(record['number'],record['name'])))
+        | 'FindingMax' >> beam.CombinePerKey(max)
+        | 'MabData' >> beam.Map(lambda record: (record[0],record[1][0],record[1][1])))
+        output = lines | 'Format' >> beam.Map(formatout)
+        output | 'WriteToText' >> beam.io.WriteToText(known_args.output)#'gs://temp_data_exam/output_folder/output', file_name_suffix='.csv',num_shards=1,header='year,number,name')
 
-
-with beam.Pipeline(options=PipelineOptions()) as p:
-    lines = (p | 'ReadData' >> beam.io.ReadFromAvro('gs://temp_data_exam/usa.avro')
-    | 'MapData' >> beam.Map(lambda record: (record['year'],(record['number'],record['name'])))
-    | 'FindingMax' >> beam.CombinePerKey(max)
-    | 'MabData' >> beam.Map(lambda record: (record[0],record[1][0],record[1][1])))
-    output = lines | 'Format' >> beam.Map(formatout)
-    output | 'WriteToText' >> beam.io.WriteToText('gs://temp_data_exam/output_folder/output', file_name_suffix='.csv',num_shards=1,header='year,number,name')
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
