c9a04e64f01455d04f7201ea6bf7ff26b087cbf8
borispmw <borispmw@gmail.com>
2021-03-26 19:55:53 +0200

    upd some fixes

--- pipeline.py
+++ pipeline.py
@@ -18,7 +18,7 @@ def run(argv=None, save_main_session=True):
     known_args, pipeline_args = parser.parse_known_args(argv)
 
     def formatout(x):
-        return '{},{},{}'.format(x[0],x[1],x[2])
+        return '{},{}'.format(x[0],x[2])
 
 
     with beam.Pipeline(options=PipelineOptions(pipeline_args)) as p:
@@ -27,7 +27,7 @@ def run(argv=None, save_main_session=True):
         | 'FindingMax' >> beam.CombinePerKey(max)
         | 'MabData' >> beam.Map(lambda record: (record[0],record[1][0],record[1][1])))
         output = lines | 'Format' >> beam.Map(formatout)
-        output | 'WriteToText' >> beam.io.WriteToText(known_args.output)#'gs://temp_data_exam/output_folder/output', file_name_suffix='.csv',num_shards=1,header='year,number,name')
+        output | 'WriteToText' >> beam.io.WriteToText(known_args.output, file_name_suffix='.csv',num_shards=1,header='year,name')#'gs://temp_data_exam/output_folder/output', file_name_suffix='.csv',num_shards=1,header='year,number,name')
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
